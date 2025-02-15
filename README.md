c2a9bc7bfc2694882fb59d80380612070eaa2d0f
astanabe <akifumi.tanabe@gmail.com>
2025-02-16 01:18:08 +0900

    The first commit.

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# wp-simple-content-control
-A simple content visibility control plugin for WordPress
+# wp-simple-contents-control
+A simple contents visibility control plugin for WordPress
+## Available shortcodes
+### [if-logout] contents [/if-logout]
+The inner contents are displayed for logged-out users only.
+### [if-login] contents [/if-logout]
+The inner contents are displayed for logged-in users only.
+### [if-user is="foo, bar"] contents [/if-user]
+The inner contents are displayed for the user "foo" or "bar" only.
+### [if-role is="administrator, editor"] contents [/if-role]
+The inner contents are displayed for the users which belongs to "administrator" or "editor" role only.
+### [if-group is="group-a, group-b"] contents [/if-group]
+The inner contents are displayed for the users which belongs to BuddyPress group "group-a" or "group-b" only.
+### [search-form]
+Output search form.
---
+++ wp-simple-contents-control.php
@@ -0,0 +1,110 @@
+<?php
+/**
+ * Plugin Name:       Simple Contents Control
+ * Plugin URI:        https://github.com/astanabe/wp-simple-contents-control
+ * Description:       A simple contents visibility control plugin for WordPress
+ * Author:            Akifumi S. Tanabe
+ * Author URI:        https://github.com/astanabe
+ * License:           GNU General Public License v2
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       wp-simple-contents-control
+ * Domain Path:       /languages
+ * Version:           0.1.0
+ * Requires at least: 6.4
+ *
+ * @package           WP_Simple_Contents_Control
+ */
+
+// Security check
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+// Add shortcode for logged-out users
+function wp_simple_contents_control_if_logout($atts, $content = null) {
+	if (!is_user_logged_in()) {
+		return do_shortcode(shortcode_unautop($content));
+	}
+	return '';
+}
+add_shortcode('if-logout', 'wp_simple_contents_control_if_logout');
+
+// Add shortcode for logged-in users
+function wp_simple_contents_control_if_login($atts, $content = null) {
+	if (is_user_logged_in()) {
+		return do_shortcode(shortcode_unautop($content));
+	}
+	return '';
+}
+add_shortcode('if-login', 'wp_simple_contents_control_if_login');
+
+// Add shortcode for specific user_login
+function wp_simple_contents_control_if_user($atts, $content = null) {
+	if (!is_user_logged_in()) {
+		return '';
+	}
+	$atts = shortcode_atts(array('is' => ''), $atts, 'if-user');
+	$users = array_map('trim', explode(',', strtolower($atts['is'])));
+	$user = wp_get_current_user();
+	if (array_intersect($users, $user->user_login)) {
+		return do_shortcode(shortcode_unautop($content));
+	}
+	return '';
+}
+add_shortcode('if-user', 'wp_simple_contents_control_if_user');
+
+// Add shortcode for specific roles
+function wp_simple_contents_control_if_role($atts, $content = null) {
+	if (!is_user_logged_in()) {
+		return '';
+	}
+	$atts = shortcode_atts(array('is' => ''), $atts, 'if-role');
+	$roles = array_map('trim', explode(',', strtolower($atts['is'])));
+	$roles = array_map(function($role) {
+		return preg_replace('/ +/', '-', $role);
+	}, $roles);
+	$user = wp_get_current_user();
+	$user_roles = (array) $user->roles;
+	if (array_intersect($roles, $user_roles)) {
+		return do_shortcode(shortcode_unautop($content));
+	}
+	return '';
+}
+add_shortcode('if-role', 'wp_simple_contents_control_if_role');
+
+// Add shorcode for specific groups
+function wp_simple_contents_control_if_group($atts, $content = null) {
+	if (!is_user_logged_in()) {
+		return '';
+	}
+	$atts = shortcode_atts(array('is' => ''), $atts, 'if-group');
+	$groups = array_map('trim', explode(',', strtolower($atts['is'])));
+	$groups = array_map(function($group) {
+		return preg_replace('/ +/', '-', $group);
+	}, $groups);
+	$user_id = get_current_user_id();
+	if (!function_exists('groups_get_user_groups')) {
+		return '';
+	}
+	$user_groups = groups_get_user_groups($user_id);
+	$user_group_ids = $user_groups['groups'];
+	foreach ($groups as $group_name) {
+		$group = groups_get_group(array('group_id' => 0, 'search_terms' => $group_name, 'per_page' => 1));
+		if (!empty($group) && isset($group['groups'][0]->id)) {
+			$group_id = $group['groups'][0]->id;
+			if (in_array($group_id, $user_group_ids)) {
+				return do_shortcode(shortcode_unautop($content));
+			}
+		}
+	}
+	return '';
+}
+add_shortcode('if-group', 'wp_simple_contents_control_if_group');
+
+// Add shortcode for inserting search form
+function wp_simple_contents_control_search_form() {
+	ob_start();
+	get_search_form();
+	return ob_get_clean();
+}
+add_shortcode('search-form', 'wp_simple_contents_control_search_form');
